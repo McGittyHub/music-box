e7edbacf301d0572d459ac5369accc2a33f5f6c1
snarf <snarf95@hotmail.com>
2020-11-30 19:57:18 +0100

    implemented voice adsr release

--- src/synth.rs
+++ src/synth.rs
@@ -6,6 +6,7 @@ struct Voice {
     key: Note,
     velocity: f32,
     time: f32,
+    release_time: Option<f32>,
 }
 
 pub struct Synth {
@@ -22,22 +23,29 @@ struct ADSR {
     release: f32,
 }
 
-fn lin_lerp(a: f32, b: f32, t: f32) -> f32 {
+fn lin_lerp(a: f32, b: f32, mut t: f32) -> f32 {
+    if t < 0.0 {
+        t = 0.0;
+    }
+    if t > 1.0 {
+        t = 0.0;
+    }
     a * (1.0 - t) + b * t
 }
 
 impl ADSR {
-    pub fn evaluate(&self, t: f32, down: bool) -> f32 {
-        if down {
-            if t < self.attack {
-                lin_lerp(0.0, 1.0, t / self.attack)
-            } else if t < self.attack + self.decay {
-                lin_lerp(1.0, self.sustain, t / (self.attack + self.decay))
-            } else {
-                self.sustain
-            }
+    pub fn evaluate(&self, t: f32, t2: f32, down: bool) -> f32 {
+        let v = if t < self.attack {
+            lin_lerp(0.0, 1.0, t / self.attack)
+        } else if t < self.attack + self.decay {
+            lin_lerp(1.0, self.sustain, t / (self.attack + self.decay))
         } else {
-            lin_lerp(1.0, 0.0, t / self.release)
+            self.sustain
+        };
+        if down {
+            v
+        } else {
+            lin_lerp(v, 0.0, t2 / self.release)
         }
     }
 }
@@ -67,9 +75,31 @@ impl Synth {
         let mut sample = 0.0;
         for voice in &self.keys_pressed {
             let freq = voice.key.to_freq_f32();
-            let vol = adsr.evaluate(self.time - voice.time, true) * voice.velocity;
+
+            let vol = if let Some(release_time) = voice.release_time {
+                adsr.evaluate(self.time - voice.time, self.time - release_time, false)
+                    * voice.velocity
+            } else {
+                adsr.evaluate(self.time - voice.time, 0.0, true) * voice.velocity
+            };
+
             sample += (self.sample_clock * freq * 2.0 * PI / self.sample_rate).sin() * vol;
         }
+
+        // Remove fully released voices
+        let time = self.time;
+        self.keys_pressed.retain(|v| {
+            if let Some(release_time) = v.release_time {
+                if time - release_time < adsr.release {
+                    true
+                } else {
+                    false
+                }
+            } else {
+                true
+            }
+        });
+
         sample
     }
 
@@ -80,11 +110,13 @@ impl Synth {
                 key,
                 velocity: vel,
                 time: self.time,
+                release_time: None,
             }
         });
     }
 
     pub fn toggle_key_up(&mut self, key: Note) {
-        self.keys_pressed.retain(|v| v.key != key);
+        let v = self.keys_pressed.iter_mut().find(|v| v.key == key).unwrap();
+        v.release_time = Some(self.time);
     }
 }
